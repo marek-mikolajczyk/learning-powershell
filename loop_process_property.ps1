d3ed818f7f97b5bd7bbd2cdf4c327b8ae3f0c4cc
Marek Mikolajczyk <lalala@localhost.loca>
2022-08-22 23:13:55 +0200

    update

--- loop_process_property.ps1
+++ loop_process_property.ps1
@@ -3,3 +3,7 @@ get-process notepad* | Select -ExpandProperty "Name" | Out-File -FilePath C:\Use
 get-content -Path C:\Users\pmare\Downloads\Process.txt | foreach-object {
     write-output "my process in $_"
     }
+     
+write-output "now trying with collection"
+
+stop-process -Name (get-content -Path C:\Users\pmare\Downloads\Process.txt)
